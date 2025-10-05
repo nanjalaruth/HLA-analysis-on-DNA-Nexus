817fed42d925bee9162278439e5d1b4a86cfbe3e
Nanjala Ruth <55382239+nanjalaruth@users.noreply.github.com>
2025-10-05 10:29:50 +0100

    Update combineHLA_LA.sh

--- combineHLA_LA.sh
+++ combineHLA_LA.sh
@@ -13,5 +13,10 @@ for i in {1..4}; do sed -i 's/^/0\t/' test2 ; done
 #add the sample name column twice
 for i in {1..4}; do sed -i 's/^/"${dataset}"\t/' test3 ; done 
 
-# Combine all the output
-cat test3 >> H3A.hped
+# Combine the output
+cat test3 >> MXL_sample_1.hped
+
+# Combine all the ped files
+cat *.ped | \
+    sed '1 i FID\\tIID\\tPID\\tMID\\tSEX\\tPHENO\\tA.1\\tA.2\\tB.1\\tB.2\\tC.1\\tC.2\\tDQA1.1\\tDQA1.2\\tDQB1.1\\tDQB1.2\\tDRB1.1\\tDRB1.2\\tDPA1.1\\tDPA1.2\\tDPB1.1\\tDPB1.2\\tDRB3.1\\tDRB3.2\\tDRB4.1\\tDRB4.2\\tE.1\\tE.2\\tF.1\\tF.2\\tG.1\\tG.2\\tPop' > MXL.hped
+	
